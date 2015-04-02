7a816bbd36b57d097e6877bb763bcc0a3aff7f13
Satu Salekari <satu.salekari@sportsetter.com>
2015-04-02 16:36:11 +0300

    Allow name to be set when creating user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
   after_filter :set_csrf_cookie_for_ng
 
   def set_csrf_cookie_for_ng
@@ -14,6 +16,10 @@ class ApplicationController < ActionController::Base
   # In Rails 4.2 and above
   def verified_request?
     super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 
 end
