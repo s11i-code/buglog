c9f413c2168849c3c23082e6821208743a0ffeec
Satu Salekari <satu.salekari@sportsetter.com>
2015-04-06 02:00:38 +0300

    Little formatting

--- app/assets/javascripts/app/services.js
+++ app/assets/javascripts/app/services.js
@@ -1,11 +1,8 @@
 angular.module('BugLog.services', [])
 
-.factory('BugType', function($resource) {
-    return $resource('/api/v1/bug_types/:id', { id: '@_id' }, {
-        update: {
-            method: 'PUT'
-        },
-        delete: { method: 'DELETE', params: {id: '@id'} }
-
-    });
-})
+    .factory('BugType', function ($resource) {
+        return $resource('/api/v1/bug_types/:id', { id: '@_id' }, {
+            update: { method: 'PUT'},
+            delete: { method: 'DELETE', params: {id: '@id'} }
+        });
+    })
