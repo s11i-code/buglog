a82c6559794539ad0be4431359609942add84ef3
Satu Salekari <satu.salekari@sportsetter.com>
2015-04-07 00:19:18 +0300

    Add function for adding alerts
    Also clear other alerts when adding new.

--- app/assets/javascripts/app/controllers.js
+++ app/assets/javascripts/app/controllers.js
@@ -3,6 +3,11 @@ angular.module('BugLog.controllers', [])
     .controller('HomeController',
     function ($scope, BugType, Vote, $interpolate) {
 
+        $scope.addAlert = function(type, message){
+            $scope.alerts = []
+            $scope.alerts.push({type: type, msg: message })
+        }
+
         $scope.alerts = []
 
         $scope.bug_types = BugType.query()
@@ -31,7 +36,6 @@ angular.module('BugLog.controllers', [])
         $scope.resetBugType = function (changed_bug_type) {
             var bug_type = BugType.get({ id: changed_bug_type.id }, function () {
                 var index = $scope.bug_types.indexOf(changed_bug_type)
-                console.log(bug_type)
                 $scope.bug_types[index] = bug_type
             });
         }
@@ -41,13 +45,13 @@ angular.module('BugLog.controllers', [])
             bug_type.$save(function () {
                 bug_type.owned_by_current_user = true
                 $scope.bug_types.push(bug_type)
-                $scope.alerts.push({type: "success", msg: $interpolate('Saved bug type {{ name }}.')(bug_type)})
+                $scope.addAlert("success", $interpolate('Saved bug type {{ name }}.')(bug_type))
             })
         }
 
         $scope.updateBugType = function (bug_type) {
             BugType.update({ id: bug_type.id }, bug_type, function () {
-                $scope.alerts.push({type: "success", msg: $interpolate('Saved bug type {{ name }}.')(bug_type) });
+                $scope.addAlert("success", $interpolate('Saved bug type {{ name }}.')(bug_type))
             })
         }
 
@@ -57,7 +61,7 @@ angular.module('BugLog.controllers', [])
                 BugType.delete({id: bug_type.id}, function () {
                     var index = $scope.bug_types.indexOf(bug_type);
                     $scope.bug_types.splice(index, 1)
-                    $scope.alerts.push({type: "success", msg: $interpolate('Deleted bug type {{ name }}.')(bug_type)})
+                    $scope.addAlert("success", $interpolate('Deleted bug type {{ name }}.')(bug_type))
                 })
             }
         }
