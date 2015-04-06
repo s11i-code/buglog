448a0a5654589dd8616664dff2fc67b605f850cc
Satu Salekari <satu.salekari@sportsetter.com>
2015-04-06 22:35:33 +0300

    Enforce ssl in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Rails.application.configure do
       enable_starttls_auto: true
   }
 
+  config.force_ssl = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
