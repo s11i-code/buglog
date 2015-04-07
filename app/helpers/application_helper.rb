035feecff53797a7548b40fc26cb6f4188d55082
Satu Salekari <satu.salekari@sportsetter.com>
2015-04-07 19:20:39 +0300

    Make partial into a helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  def github_sign_in_button
+    link_to user_omniauth_authorize_path(provider: :github), class: ['btn btn-block btn-lg btn-social btn-github'] do
+      content_tag(:i, '', class: 'fa fa-github') + 'Sign in with GitHub'
+    end
+  end
 end
